09c627942cf05ecb21e0ca088df4b5eafa936444
bencw12 <bholmes@vassar.edu>
2023-02-16 11:50:38 -0500

    scripts: add building qemu to build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(dirname $0)"
-KERNELS_DIR="${SCRIPT_DIR}/../kernels"
-SRC_TREE_DIR="${SCRIPT_DIR}/../src"
-LINUX_SRC_DIR="${SRC_TREE_DIR}/linux"
-LINUX_SRC_URL="https://github.com/severifast/linux"
-CONFIGS_DIR="${SCRIPT_DIR}/../kernel-configs"
-    
+. ./scripts/common
+
 build_kernels()
 {
     ! [ -d ${LINUX_SRC_DIR} ] && {
-	git clone --single-branch --branch tracepoints ${LINUX_SRC_URL} ${LINUX_SRC_DIR}
+	git clone --single-branch --branch\
+	    tracepoints ${LINUX_SRC_URL} ${LINUX_SRC_DIR}
     }
 
     ! [ -d ${KERNELS_DIR} ] && {
@@ -22,20 +18,37 @@ build_kernels()
 	    cp ${CONFIGS_DIR}/${config}\
 	       ${LINUX_SRC_DIR}/.config
 
+	    pushd ${LINUX_SRC_DIR}
 	    make -C ${LINUX_SRC_DIR} olddefconfig
 	    make -C ${LINUX_SRC_DIR}\
 		 -j$(getconf _NPROCESSORS_ONLN) bzImage
+	    popd
 
-	    cp ${LINUX_SRC_DIR}/arch/x86/boot/bzImage\
+	    cp ${BZIMAGE_BUILD}\
 	       ${KERNELS_DIR}/bzImage-${config%.*}-lz4
 
-	    cp ${LINUX_SRC_DIR}/arch/x86/boot/compressed/vmlinux.bin ${KERNELS_DIR}/vmlinux-${config%.*}
+	    cp ${VMLINUX_BUILD}\
+	       ${KERNELS_DIR}/vmlinux-${config%.*}
 
 	done
     }
     	     
 }
 
-# build_qemu
-# build_ovmf
+build_qemu()
+{
+    ! [ -d ${QEMU_SRC_DIR} ] && {
+	git clone --single-branch --branch\
+	    tracepoints ${QEMU_SRC_URL} ${QEMU_SRC_DIR}
+    }
+
+    ! [ -f ${QEMU_BUILD} ] && {
+	pushd ${QEMU_SRC_DIR}
+	./configure --target-list=x86_64-softmmu
+	make -j$(getconf _NPROCESSORS_ONLN)
+	popd
+    }
+}
+
 build_kernels
+build_qemu
