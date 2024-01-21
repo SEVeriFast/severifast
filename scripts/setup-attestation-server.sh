1ac3433a55baa32850def47b8dbdd88174cf878a
Ben Holmes <bcwh@csail.mit.edu>
2024-01-21 16:05:42 +0000

    setup log for attestation server

--- scripts/setup-attestation-server.sh
+++ scripts/setup-attestation-server.sh
@@ -14,6 +14,9 @@ sudo chown www-data:www-data ${KEYS_DIR}
 sudo chown www-data:www-data ${CGI_BIN}
 sudo chown www-data:www-data ${CERTS_DIR}
 
+sudo touch /var/www/cgi.log
+sudo chown www-data:www-data /var/www/cgi.log
+
 sudo cp ${ROOT_DIR}/attestation/http.conf /etc/nginx/sites-enabled/
 sudo cp ${ROOT_DIR}/attestation/disk-key.sh ${CGI_BIN}
 # we know attestation succeeded if /bin/signal runs because the path is the "key" given when attestation succeeds
