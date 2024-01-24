fd56ec33e14e0c76a32538fe70422acd40b1efbf
Ben Holmes <bcwh@csail.mit.edu>
2024-01-24 00:35:25 +0000

    do network setup in setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,5 +6,8 @@ sudo ${SCRIPT_DIR}/cfg_groups.sh
 # set up attestation server
 sudo ${ROOT_DIR}/scripts/setup-attestation-server.sh
 
+# set up tap device for VMs and start attestation server
+sudo ${ROOT_DIR}/scripts/network-setup.sh
+
 # enable huge pages for sev-snp
 echo "always" | sudo tee /sys/kernel/mm/transparent_hugepage/shmem_enabled > /dev/null
